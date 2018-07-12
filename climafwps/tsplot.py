02c849c14bb8dcbd453299b3ef6a52008ae180f2
Pingu Carsti <ehbrecht@dkrz.de>
2018-07-12 15:58:52 +0200

    updated climaf init

--- climafwps/tsplot.py
+++ climafwps/tsplot.py
@@ -9,14 +9,16 @@ from CMIP5.
 import os
 import tempfile
 
+# init climaf
+os.environ['CLIMAF_CACHE'] = os.path.join(tempfile.gettempdir(), 'climaf_cache')
+from climaf.api import ds, cfile, plot, space_average
+
 
 def create_global_mean_ts_plot(model, experiment, start_year, end_year, variable,
                                output='output.png'):
     """
     Create a global average time series of CMIP5 variable and plot to output.
     """
-    os.environ['CLIMAF_CACHE'] = os.path.join(tempfile.gettempdir(), 'climaf_cache')
-    from climaf.api import ds, cfile, plot, space_average
     dset = ds(project='CMIP5', model=model, experiment=experiment, frequency='monthly',
               period='{}-{}'.format(start_year, end_year), variable=variable)
     gmean = space_average(dset)
